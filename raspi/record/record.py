c52aa2d1fb4298451068f8bb529988f25df73715
Andrew Caylor <andrewc01@vt.edu>
2023-02-20 23:59:35 -0500

    added wav filtering function

--- raspi/record/record.py
+++ raspi/record/record.py
@@ -1,8 +1,12 @@
 import os
-    
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+
+relativePath = "recording.wav"
+
 #records audio and writes to wav file. returns path to file
 def recordWAV():
-    relativePath = "recording.wav"
     
     # Read all devices that can record audio
     os.system("arecord -l > temp.txt")
@@ -16,7 +20,7 @@ def recordWAV():
         searchStr = "card " + str(i)
         # If we can record from a device, record from the first one we find
         if(searchStr in data):
-            os.system("arecord -Dplughw:" + str(i) + ",0 --duration=2 " + relativePath)
+            os.system("arecord -Dplughw:" + str(i) + ",0 -f S16_LE --duration=5 -r8000 " + relativePath)
             found = True
             break
     
@@ -29,10 +33,45 @@ def recordWAV():
     else:
         print("Error finding microphone")
 
+def freqFilter(signal):
+    freq = np.fft.fftfreq(signal.size, d=1/8000)
+    lowpass = np.abs(freq) < 400
+    
+    fourier = np.fft.fft(signal.reshape(40000,))
+    filtered = fourier * lowpass
+    back = np.real(np.fft.ifft(filtered))
+    
+    return back
+
+def filterWAV(path):
+    filteredPath = "filtered.wav"
+    with wave.open(path) as f:
+        buffer = f.readframes(f.getnframes())
+        interleaved = np.frombuffer(buffer, dtype=f'int{f.getsampwidth()*8}')
+        data = np.reshape(interleaved, (-1, f.getnchannels()))
+        
+        filtered = freqFilter(data)
+        
+        maxsize = (2**16)/3
+        datamax = np.max(filtered)
+
+        amped = ((maxsize/datamax) * filtered).astype(int)
+        
+        with wave.open(filteredPath, "w") as outf:
+            outf.setnchannels(1)
+            outf.setsampwidth(2)
+            outf.setframerate(8000)
+            outf.writeframes(amped.astype(np.short).tobytes())
+            
+            return filteredPath
+        
+
 #records ECG and writes to csv file
 def recordECG():
     #TODO implement this function for real
     return "ecg.csv"
     
 if __name__ == "__main__":
-    recordWav()
+    print(2 ** 16)
+    path = recordWAV()
+    filterWAV(path)
