7eb827b1aa83ba5fa913066a87d4ef721bcc64e8
Andrew Caylor <andrewc01@vt.edu>
2023-04-13 20:17:54 -0400

    fixed ecg corruption

--- raspi/main.py
+++ raspi/main.py
@@ -7,21 +7,33 @@ sys.path.insert(0,"./upload")
 # start reading from serial port
 import os
 import threading
+import time
 
 # reads from the serial port and writes to a file
-# keeps reading until program is terminated
+# keeps reading until... NEVER
 def beginRead():
-  if os.path.exists("rawECG.txt"):
-      os.remove("rawECG.txt")
   # kill any other processes that are reading from the serial port
   os.system("sudo pkill -f cat")
+  
+  # wait to allow previous read to finish writing
+  # removing the file while cat is writing to the file causes corruption
+  time.sleep(.3)
+  
+  # remove the file so we get a fresh start
+  if os.path.exists("rawECG.txt"):
+      os.remove("rawECG.txt")
+      
   # make sure we have permission to read from the serial port
   os.system("sudo chmod a+rw /dev/ttyACM0")
+  
   # set the baud rate
-  os.system("stty -F /dev/ttyACM0 460800")
+  # higher baudrates cause rare data corrputions
+  os.system("stty -F /dev/ttyACM0 115200")
+  
   # start reading from the serial port
   os.system("cat /dev/ttyACM0 >> rawECG.txt")
-# start reading from the serial port
+  
+# read from that serial port
 thread = threading.Thread(target=beginRead)
 thread.start()
 
