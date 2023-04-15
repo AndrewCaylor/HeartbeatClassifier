06358ae9d28eef31ab59b7646a023c5cb30e7ced
Andrew <andrewc01@vt.edu>
2023-04-14 20:48:19 -0400

    arduino code cleanup

--- arduino/test.py
+++ arduino/test.py
@@ -1,17 +1,14 @@
 # start reading from serial port
 import os
-import threading
 
 # reads from the serial port and writes to a file
 # keeps reading until program is terminated
-def beginRead():
-  # make sure the file is empty before we start
-  os.system("cat /dev/null > rawECG.txt")
-  # make sure we have permission to read from the serial port
-  os.system("sudo chmod a+rw /dev/ttyACM0")
-  # set the baud rate
-  os.system("stty -F /dev/ttyACM0 460800")
-  # start reading from the serial port
-  os.system("cat /dev/ttyACM01 > rawECG.txt")
+
+# make sure the file is empty before we start
+os.system("cat /dev/null > rawECG.txt")
+# make sure we have permission to read from the serial port
+os.system("sudo chmod a+rw /dev/ttyACM0")
+# set the baud rate
+os.system("stty -F /dev/ttyACM0 115200")
 # start reading from the serial port
-threading.Thread(target=beginRead).start()
+os.system("cat /dev/ttyACM01 > rawECG.txt")
---
+++ arduino/test/adc.ino
@@ -0,0 +1,19 @@
+int analogPin = A0;
+int val = 0;
+
+// Choose 115200 baudrate
+// Data will be corrupted by Ras Pi if baudrate is too high
+// Higher ones seem to work fine on my Desktop for some reason
+void setup() {
+  Serial.begin(115200);
+}
+
+// Read analog value from A0 and print it to serial
+// Print the time in milliseconds since the program started too
+void loop() {
+  Serial.print(millis());
+  Serial.print(" ");
+  val = analogRead(A0);
+  Serial.print(String(val));
+  Serial.print("\n");
+}
